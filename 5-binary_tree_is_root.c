f4e2969baa0b19da79cef3aa76e1c228db345ada
root <sylyac2000@gmail.com>
2022-08-25 21:21:10 -0700

    Write a function that checks if a given node is a root updated

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -5,7 +5,7 @@
  * @node: pointer to the node to check
  * Return: 1 if root else 0
  */
-int binary_tree_is_root(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
